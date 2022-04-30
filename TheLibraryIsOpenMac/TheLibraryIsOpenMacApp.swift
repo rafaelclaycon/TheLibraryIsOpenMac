c01de21cebf0fa4fc66cdd9d7f40523b6398597c
Rafael Schmitt <rafael.claycon@gmail.com>
2022-04-30 19:17:11 -0300

    Initial Commit

---
+++ TheLibraryIsOpenMac/TheLibraryIsOpenMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLibraryIsOpenMacApp.swift
+//  TheLibraryIsOpenMac
+//
+//  Created by Rafael Schmitt on 30/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TheLibraryIsOpenMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
